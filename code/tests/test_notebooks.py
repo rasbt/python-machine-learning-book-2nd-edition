8e01f5a39d878333840b7fd606eff417250d8621
rasbt <mail@sebastianraschka.com>
2017-02-09 10:38:53 -0500

    basic setup

--- code/tests/test_notebooks.py
+++ code/tests/test_notebooks.py
@@ -3,16 +3,20 @@ import os
 import subprocess
 import tempfile
 import watermark
-import nbformat
+import sys
 
 
 def run_ipynb(path):
+    if (sys.version_info >= (3, 0)):
+        kernel_name = 'python3'
+    else:
+        kernel_name = 'python2'
     error_cells = []
     with tempfile.NamedTemporaryFile(suffix=".ipynb") as fout:
-        args = ["python", "-m", "nbconvert", "--to",
-                "notebook", "--execute", "--output",
-                "--ExecutePreprocessor.kernel_name", "python",
-                fout.name, path]
+        args = ["jupyter", "nbconvert", "--to",
+                "notebook", "--execute",
+                "--ExecutePreprocessor.kernel_name=%s" % kernel_name,
+                "--output", fout.name, path]
         subprocess.check_output(args)
 
 
