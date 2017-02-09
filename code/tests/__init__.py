bb60b8a27e4df097d898297a02f982427149677a
rasbt <mail@sebastianraschka.com>
2017-02-09 01:10:39 -0500

    fail tests on purpose

---
+++ code/tests/__init__.py
@@ -0,0 +1,9 @@
+# Sebastian Raschka 2016-2017
+#
+# ann is a supporting package for the book
+# "Introduction to Artificial Neural Networks and Deep Learning:
+#  A Practical Guide with Applications in Python"
+#
+# Author: Sebastian Raschka <sebastianraschka.com>
+#
+# License: MIT
