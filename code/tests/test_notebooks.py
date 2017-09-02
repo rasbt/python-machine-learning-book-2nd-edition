85ec59973d80823998edac01a1326d341d28618e
rasbt <mail@sebastianraschka.com>
2017-09-02 00:42:10 -0400

    add more generous timeout

--- code/tests/test_notebooks.py
+++ code/tests/test_notebooks.py
@@ -14,6 +14,7 @@ def run_ipynb(path):
     with tempfile.NamedTemporaryFile(suffix=".ipynb") as fout:
         args = ["jupyter", "nbconvert", "--to",
                 "notebook", "--execute",
+                "--ExecutePreprocessor.timeout=1000",
                 "--ExecutePreprocessor.kernel_name=%s" % kernel_name,
                 "--output", fout.name, path]
 
