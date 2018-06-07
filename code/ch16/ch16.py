749772627d4ab00705713ff802b6fd9ede5e70cc
rasbt <mail@sebastianraschka.com>
2018-06-07 11:16:46 -0400

    rename tot_batch_length to mini_batch_length for clarity

--- code/ch16/ch16.py
+++ code/ch16/ch16.py
@@ -495,19 +495,19 @@ if 'TRAVIS' in os.environ:
 
 
 def reshape_data(sequence, batch_size, num_steps):
-    tot_batch_length = batch_size * num_steps
-    num_batches = int(len(sequence) / tot_batch_length)
-    if num_batches*tot_batch_length + 1 > len(sequence):
+    mini_batch_length = batch_size * num_steps
+    num_batches = int(len(sequence) / mini_batch_length)
+    if num_batches*mini_batch_length + 1 > len(sequence):
         num_batches = num_batches - 1
     ## Truncate the sequence at the end to get rid of 
     ## remaining charcaters that do not make a full batch
-    x = sequence[0 : num_batches*tot_batch_length]
-    y = sequence[1 : num_batches*tot_batch_length + 1]
+    x = sequence[0 : num_batches*mini_batch_length]
+    y = sequence[1 : num_batches*mini_batch_length + 1]
     ## Split x & y into a list batches of sequences: 
     x_batch_splits = np.split(x, batch_size)
     y_batch_splits = np.split(y, batch_size)
     ## Stack the batches together
-    ## batch_size x tot_batch_length
+    ## batch_size x mini_batch_length
     x = np.stack(x_batch_splits)
     y = np.stack(y_batch_splits)
     
